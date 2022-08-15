c6f99c6f6bd4aa575b10af30ffad617c7d7b70fc
Dima <diman-pichugin@yandex.ru>
2022-08-15 13:34:01 +0300

    Initial commit

---
+++ src/main/java/com/weblinestudio/mypoint/MyPointApplication.java
@@ -0,0 +1,13 @@
+package com.weblinestudio.mypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPointApplication.class, args);
+	}
+
+}
